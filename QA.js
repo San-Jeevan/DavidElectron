c10c799d5d351b7351b12d37f4c94ac40b7ad9d5
DavidHaavik <87383310+DavidHaavik@users.noreply.github.com>
2021-09-02 17:43:50 +0200

    kodeendringer i løpet av sesjon

--- QA.js
+++ QA.js
@@ -7,7 +7,7 @@ questions[2] = []; //osvs...
 // MODUL 1
 var q1 = {};
 q1.stage = 0;
-q1.question = 'hva er 1+1?'; //det er lov å bruke fargekoder her som: \u001b[36mI blabla \033[0m
+q1.question = 'hva er \u001b[36m 1+1 \033[0m?'; //det er lov å bruke fargekoder her som: \u001b[36mI blabla \033[0m
 q1.questionHelpText = "1,2 eller 3"; //det er lov å bruke fargekoder her som: \u001b[36mI blabla \033[0m
 q1.answer = "2";
 q1.score = 1;
@@ -19,20 +19,28 @@ var q2 = {};
 q2.stage = 1;
 q2.question = "hva er 3 x 4";
 q2.questionHelpText = "(9, 16, 12)";
-q2.answer = "12";
-q2.score = 3;
+q2.answer = "12;tolv";
+q2.score = 1;
 
 questions[0].push(q2);
 
+var q3 = {};
+q3.stage = 2;
+q3.question = "Hva er 2+2?";
+q3.questionHelpText = "";
+q3.answer = "4;fire;four"; //seerator ;
+q3.score = 1;
+
+questions[0].push(q3);
 
 
 
 // MODUL 2
-var q3 = {};
-q3.stage = 2;
-q3.question = "Jostein sover i sofaen  - Jostein is sleeping in the car";
-q3.questionHelpText = "Her skal man svare korrekt eller ikke korrekt";
-q3.answer = "ikke korrekt";
-q3.score = 5;
+var q4 = {};
+q4.stage = 2;
+q4.question = "Jostein sover i sofaen  - Jostein is sleeping in the car";
+q4.questionHelpText = "Her skal man svare korrekt eller ikke korrekt";
+q4.answer = "ikke korrekt";
+q4.score = 1;
 
-questions[1].push(q3);
+questions[1].push(q4);
--- renderer.js
+++ renderer.js
@@ -17,17 +17,30 @@ window.addEventListener('resize', function (event) {
 //vi disabler pil opp og ned tastene
 term.attachCustomKeyEventHandler(function (e) {
     if (e.code == "ArrowUp" || e.code == "ArrowDown") {
-      return false;
+      //return false;
     }
-  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //bruker innstillinger
 var score = 0;
-var currentStage = 0;
+var currentStage = 0; //dette kan du se på som spørsmål 0
 var currentModule = 0;
+var maxFailedAttempts = 1000;
 var failedAttempts = 0;
-var maxFailedAttempts = 2;
 var currentAnswer = "";
 var gameisOver = false;
 
@@ -51,24 +64,24 @@ function resetGame() {
 
 
 function showFinalScore() {
-    term.writeln('-----SPILLET ER SLUTT----');
+    term.writeln('-----MODULEN ER SLUTT----');
     term.writeln('DU FIKK:');
-    term.writeln(`${score} poeng`);
+    var percentage = ((score / questions[0].length) ) * 100;
+    term.writeln(`${score} / ${questions[0].length} (${percentage}%) poeng`);
     if(score < 3){
         term.writeln("\u001b[36mI know this was your first quiz, but I suggest you try again.\033[0m")
     }
     else {
         term.writeln("Well done, lets go to the next stage?");
-       
-        //koden nedenfor går til modul2 av spillet
-        // currentModule++;
-        // resetGame();
-        // print_lines(5);
-        // term.writeln(questions[currentModule][currentStage].question);
-        // if(questions[currentModule][currentStage].questionHelpText !== ""){
-        //     term.writeln(`(Hjelp: ${questions[currentModule][currentStage].questionHelpText})`);         
-        // }
     }
+    term.writeln("Press enter to continue....");
+        currentModule++;
+        resetGame();
+        print_lines(5);
+        term.writeln(questions[currentModule][currentStage].question);
+        if(questions[currentModule][currentStage].questionHelpText !== ""){
+            term.writeln(`(Hjelp: ${questions[currentModule][currentStage].questionHelpText})`);         
+        }
 }
 
 function print_lines(lines){
@@ -77,40 +90,65 @@ function print_lines(lines){
       } 
 }
 
+function isTheAnswerCorrect(userinput, questionAnswer) {
+    if (questionAnswer.indexOf(";") != -1) {
+        //its has multi-answers
+        var listOfAnswers = questionAnswer.split(';');
+        for (let i = 0; i < listOfAnswers.length; i++) {
+            if(listOfAnswers[i].toLowerCase() == userinput.toLowerCase()) return true;
+          } 
+    }
+    else {
+        return userinput.toLowerCase() == questionAnswer.toLowerCase();
+    }
+    return false;
+}
+
 
 
 // -------------------------GAMELOOP---------------------
 function gameLoop(userinput) {
     var _currentQuestion = questions[currentModule][currentStage];
-    if (userinput.toLowerCase() == _currentQuestion.answer.toLowerCase()) {
+    
+    if (isTheAnswerCorrect(userinput, _currentQuestion.answer)) {
         score = score + _currentQuestion.score;
         term.writeln('RIKTIG!!!!!!');
         term.write('\n');
-        if(isTheGameOver(currentStage)){
-            gameisOver = true;
-            showFinalScore();
-            return;
-        }
-        currentStage++;
-        var _nextQuestion = questions[currentModule][currentStage];
-        term.writeln(_nextQuestion.question);
-        if(_nextQuestion.questionHelpText !== ""){
-            term.writeln(`(Hjelp: ${_nextQuestion.questionHelpText})`);         
-        }
     }
     else {
-        term.writeln('\x1B[1;3;31mFeil!\x1B[0m');
-        failedAttempts++;
-        if((maxFailedAttempts-failedAttempts) == 0)
-        {
-            term.writeln(`-----GAME OVER-----`);
-            gameisOver = true;
-            return;
-        }
-        term.writeln(`Du har svart feil ${failedAttempts} ganger, og har ${maxFailedAttempts-failedAttempts} forsøk igjen`);
-        term.writeln(_currentQuestion.question);
+        term.writeln(`\x1B[1;3;31mDu har svart feil!\x1B[0m Korrekt svar: ${_currentQuestion.answer}`);
+        print_lines(4);
+    }
+
+    if(isTheGameOver(currentStage)){
+        gameisOver = true;
+        showFinalScore();
+        return;
+    }
+    currentStage++;
+    var _nextQuestion = questions[currentModule][currentStage];
+    term.writeln(_nextQuestion.question);
+    if(_nextQuestion.questionHelpText !== ""){
+        term.writeln(`(Hjelp: ${_nextQuestion.questionHelpText})`);         
     }
 }
+
+//     //logikk når svaret er feil
+//     else {
+//         term.writeln(`\x1B[1;3;31mDu har svart feil!\x1B[0m Korrekt svar: ${_currentQuestion.answer}}`);
+        
+
+//         failedAttempts++;
+//         if((maxFailedAttempts-failedAttempts) == 0)
+//         {
+//             term.writeln(`-----GAME OVER-----`);
+//             gameisOver = true;
+//             return;
+//         }
+//         //term.writeln(`Du har svart feil ${failedAttempts} ganger, og har ${maxFailedAttempts-failedAttempts} forsøk igjen`);
+//         //term.writeln(_currentQuestion.question);
+//     }
+// }
 // -------------------------GAMELOOP---------------------
 
 
